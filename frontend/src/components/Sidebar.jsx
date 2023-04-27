fc69cad3d24da91a0772e10d7accc975153cc713
juliocjuri <juliocesarjuriolli@gmail.com>
2023-04-27 13:17:03 -0300

    feat(frontend): added button, logo and animation to sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,17 +1,20 @@
 import React, { Component, useState } from 'react';
 import './Sidebar.css'
+import { FaBars } from 'react-icons/fa'
 
 class Sidebar extends Component {
      constructor(props){
           super(props);
           this.state = {
-               isOpened: false
+               isOpened: false,
+               rotate: false
           }
      }
 
      handleButtonClick(){
           this.setState({ 
-               isOpened: !this.state.isOpened
+               isOpened: !this.state.isOpened,
+               rotate: !this.state.rotate
           })
           console.log(this.state.isOpened)
      }
@@ -25,7 +28,20 @@ render() {
                     
                     <button onClick={() => {
                          this.handleButtonClick();
-                    }} className='collapse-btn'> </button>
+                    }} className='collapse-btn' > 
+                    <FaBars className='collapse-btn-icon'/>
+                    </button>
+                    <header className='header-wrapper'>
+                         <img 
+                              src='/src/assets/logo-check-sign.png' 
+                              alt='fgdg' 
+                              className='logo-check-sign'
+                              rotate={this.state.rotate.toString()}
+                         />
+                    </header>
+                    <main className='icons-wrapper'>
+                         
+                    </main>
                </div>
           </div>
      )
