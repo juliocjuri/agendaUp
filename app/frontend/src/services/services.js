f109e86ba57786745e393b0ee9afb7b0a9852897
juliocjuri <juliocesarjuriolli@gmail.com>
2023-05-01 20:09:32 -0300

    fix: changed ip address for ci

--- app/frontend/src/services/services.js
+++ app/frontend/src/services/services.js
@@ -1,5 +1,5 @@
-//export const api = 'http://3.95.195.130:3000/' 
-export const api = 'http://localhost:3000/' 
+export const api = 'http://54.146.233.205:3000/' 
+//export const api = 'http://localhost:3000/' 
 
 //This is the AWS EC2 instance address
 
